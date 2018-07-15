00c62d86fabee2fea80b6769cb4b9a1a6e04c347
nguyenanhtu2 <nguyenanhtuinc@gmail.com>
2018-07-15 10:11:43 +0200

    first commit

---
+++ src/main/java/TestGroupId/system/login/App.java
@@ -0,0 +1,13 @@
+package TestGroupId.system.login;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! 12345");
+    }
+}
---
+++ src/main/java/jbr/springmvc/service/UserService.java
@@ -0,0 +1,17 @@
+package jbr.springmvc.service;
+
+import jbr.springmvc.model.Login;
+import jbr.springmvc.model.User;
+
+public class UserService {
+
+	public User validateUser(Login login) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void register(User user) {
+		// TODO Auto-generated method stub
+	}
+
+}
